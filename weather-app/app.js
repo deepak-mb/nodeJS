1602cc63132489f8f9d7d03ae1f6ef2ef23ec956
Deepak <deepak.muthukulabadrinath@tesco.com>
2020-05-04 09:26:15 +0530

    Working on weather app

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,16 +1,25 @@
-const request = require('request');
+const yargs = require('yargs')
 
-const url = 'http://api.weatherstack.com/current?access_key=ff471ca924f4b52361b98890db52eb81&query=Bangalore&units=f';
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
-// request({ url: url, json: true }, (error, response) => {
-// 	// console.log(response.body.current);
-// 	const { current } = response.body;
-// 	console.log(`${current.weather_descriptions}. It is currently ${current.temperature} degrees out. It feels like ${current.feelslike} out.`)
-// });
+const address = process.argv[2];
 
-//Geocoding
-const geoUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZGVlcGFrbWIiLCJhIjoiY2s5a3NwMm5oMDFhejNvcnZxNjFpN3lqdSJ9.H1R8rPHAN8S60uiDmFEfEA&limit=1';
-request({ url: geoUrl, json: true }, (error, response) => {
-	const { features } = response.body;
-	console.log(`Latitude: ${features[0].center[1]}. Longitude: ${features[0].center[0]}`)
-});
+if (!address) {
+	console.log(`Please provide an address.`);
+} else {
+	geocode(address, (error, data) => {
+		if (error) {
+			return console.log('Error: ', error);
+		} else {
+			console.log(data.location);
+		}
+		forecast(data.latitude, data.longitude, (error, forecastData) => {
+			if (error) {
+				return console.log('Error', error);
+			} else {
+				console.log(forecastData);
+			}
+		});
+	});
+}
