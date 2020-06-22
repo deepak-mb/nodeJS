2758120879c159e890478dbe9c925de9837893d1
Deepak <deepak.muthukulabadrinath@tesco.com>
2020-06-22 09:21:29 +0530

    Added middleware

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -8,6 +8,18 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3010
 
+// app.use((req, res, next) => {
+// 	if (req.method === 'GET') {
+// 		res.send('GET requests are disabled')
+// 	} else { 
+// 		next()
+// 	}
+// })
+
+// app.use((req, res, next) => {
+// 	res.status(503).send('Requests are disabled')
+// })
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -15,3 +27,16 @@ app.use(taskRouter)
 app.listen(port, () => {
 	console.log('Server is up on port ' + port)
 })
+
+const jwt = require('jsonwebtoken')
+
+const myFunc = async () => {
+	const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', { expiresIn: '7days' })
+	// console.log(token)
+
+	const data = jwt.verify(token, 'thisismynewcourse')
+
+	console.log(data)
+}
+
+myFunc()
